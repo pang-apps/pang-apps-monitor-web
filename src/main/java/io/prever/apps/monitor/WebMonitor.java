ace04e6eaf76641d45ff73c9579554ac2d3babaf
iprever <prever@ioter.io>
2015-11-12 10:20:38 +0900

    Multiple url targets features added.

--- src/main/java/io/prever/apps/monitor/WebMonitor.java
+++ src/main/java/io/prever/apps/monitor/WebMonitor.java
@@ -22,7 +22,6 @@
 package io.prever.apps.monitor;
 
 import io.prever.sdk.Prever;
-import io.prever.sdk.callback.MultipleDataCallback;
 import io.prever.sdk.http.PreverHttp;
 import io.prever.sdk.util.PreverProperties;
 
@@ -33,7 +32,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
@@ -41,52 +46,97 @@ import org.slf4j.LoggerFactory;
 
 public class WebMonitor {
   private static final Logger logger = LoggerFactory.getLogger(WebMonitor.class);
+  private static boolean running = true;
 
   public static void main(String[] args) throws Exception {
+    //Prever must be initialized first to use prever.properties by PreverProperties
     final Prever prever = new PreverHttp();
-    final String targetUrl = (String) PreverProperties.getProperty("webmonitor.url");
-    final String devicename = (String) PreverProperties.getProperty("webmonitor.responseTime.devicename");
-    
-    prever.startTimerTask(new MultipleDataCallback() {
-      public void onSuccess(Object sent) {}
 
-      public boolean isRunning(int sentCount) {
-        return true;
-      }
+    Map<Integer, Map<String, String>> targets = extractTargets();
 
-      public Object getData() {
-        // start timer
-        long milliStart = System.currentTimeMillis();
+    final ExecutorService threadPools = Executors.newFixedThreadPool(targets.size());
 
-        // do work to be timed
-        try {
-          getResponse(targetUrl);
-        } catch (Exception e) {
-          logger.error("Response error", e);
+    Iterator<Entry<Integer, Map<String, String>>> iterator = targets.entrySet().iterator();
+    while (iterator.hasNext()) {
+      Entry<Integer, Map<String, String>> next = iterator.next();
+      final Map<String, String> target = next.getValue();
+      threadPools.execute(new Runnable() {
+
+        public void run() {
+
+          while (running ) {
+            // start timer
+            long milliStart = System.currentTimeMillis();
+
+            // do work to be timed
+            try {
+              getResponse(target.get("url"));
+              // stop timer
+              long milliEnd = System.currentTimeMillis();
+
+              // report response times
+              long milliTime = milliEnd - milliStart;
+              Map<String, Long> data = new HashMap<String, Long>();
+
+              data.put(target.get("devicename"), milliTime);
+              prever.sendData(data);
+            } catch (Exception e) {
+              logger.error("Monitor has an error", e);
+            }
+
+            try {
+              TimeUnit.SECONDS.sleep(PreverProperties.getPeriod());
+            } catch (InterruptedException e) {
+            }
+          }
         }
-        // stop timer
-        long milliEnd = System.currentTimeMillis();
 
-        // report response times
-        long milliTime = milliEnd - milliStart;
-        String rt_millitime = String.format("%,.3f", milliTime / 1.0);
-        Map<String, String> data = new HashMap<String, String>();
-        data.put(devicename, rt_millitime);
-
-        return data;
-      }
-
-      private void getResponse(String targetUrl) throws MalformedURLException, IOException {
-        URL url = new URL(targetUrl);
-        URLConnection conn = url.openConnection();
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        while (in.readLine() != null) {
+        private void getResponse(String targetUrl) throws MalformedURLException, IOException {
+          URL url = new URL(targetUrl);
+          URLConnection conn = url.openConnection();
+          BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+          while (in.readLine() != null) {
+          }
+          in.close();
         }
-        in.close();
-      }
-      
-    }, PreverProperties.getPeriod(), TimeUnit.MILLISECONDS);
 
+      });
+
+      Runtime.getRuntime().addShutdownHook(new Thread() {
+        @Override
+        public void run() {
+          running = false;
+          threadPools.shutdown();
+        }
+      });
+    }
+  }
+
+  private static Map<Integer, Map<String, String>> extractTargets() {
+    Properties properties = PreverProperties.getProperties();
+    Set<Entry<Object, Object>> entrySet = properties.entrySet();
+    Iterator<Entry<Object, Object>> iterator = entrySet.iterator();
+
+    Map<Integer, Map<String, String>> targets = new HashMap<Integer, Map<String, String>>();
+
+    while (iterator.hasNext()) {
+      Entry<Object, Object> next = iterator.next();
+      String key = (String) next.getKey();
+      if (key.startsWith("webmonitor")) {
+        String sub1 = key.substring(key.indexOf(".") + 1);
+        int indexOf = sub1.indexOf(".");
+        String number = sub1.substring(0, indexOf);
+        Map<String, String> target = targets.get(Integer.valueOf(number));
+        if (target == null) {
+          target = new HashMap<String, String>();
+          targets.put(Integer.valueOf(number), target);
+        }
+
+        target.put(sub1.substring(sub1.lastIndexOf(".") + 1).trim(), (String) next.getValue());
+      }
+    }
+
+    return targets;
   }
 
 }
